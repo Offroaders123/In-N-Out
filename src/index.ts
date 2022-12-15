e6c78b08ed18c4b7103140ff0c421bdf85c89222
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2022-12-15 01:52:50 -0800

    Drinks & Shakes + Menu Screenshots

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,18 @@ export type ChoppedOnion = "Chopped Onion";
 
 export type WholeGrilledOnion = "Whole Grilled Onion";
 export type GrilledOnion = "Grilled Onion";
+
+export type Coke = "Coke";
+export type DietCoke = "Diet Coke";
+export type SevenUp = "7-Up";
+export type DrPepper = "Dr Pepper";
+export type RootBeer = "Root Beer";
+export type IcedTea = "Iced Tea";
+export type PinkLemonade = "Pink Lemonade";
+export type LemonadeZeroSugar = "Lemonade Zero Sugar";
+
+export type Shake = ChocolateShake | StrawberryShake | VanillaShake;
+
+export type ChocolateShake = "Chocolate Shake";
+export type StrawberryShake = "Strawberry Shake";
+export type VanillaShake = "Vanilla Shake";
