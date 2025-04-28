009f2e9866ad8c4eb4f5cb86c04948388ac85334
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-04-27 20:55:52 -0600

    Burgie Formatting

--- src/DoubleDouble.ts
+++ src/DoubleDouble.ts
@@ -1,6 +1,30 @@
 import type { Bunnable, Toasted } from "./Bunnable.js";
 import type { Cheesable } from "./Cheesable.js";
 import type { Meatable } from "./Meatable.js";
-import type { Chilliable, ChoppedOnionable, GrilledOnionable, Lettuceable, Onionable, RawChoppedOnionable, RawOnionable, Spreadable, Tomatoeable, WholeGrilledOnionable } from "./Topping.js";
+import type {
+  Chilliable,
+  ChoppedOnionable,
+  GrilledOnionable,
+  Lettuceable,
+  Onionable,
+  RawChoppedOnionable,
+  RawOnionable,
+  Spreadable,
+  Tomatoeable,
+  WholeGrilledOnionable
+} from "./Topping.js";
 
-export interface DoubleDouble extends Bunnable<Toasted>, Cheesable<2 | 3 | 4>, Meatable<2 | 3 | 4>, Lettuceable, Tomatoeable, Spreadable, Partial<Chilliable>, Partial<GrilledOnionable>, Partial<Onionable>, Partial<WholeGrilledOnionable>, Partial<RawOnionable>, Partial<RawChoppedOnionable>, Partial<ChoppedOnionable> {}
+export interface DoubleDouble extends
+  Bunnable<Toasted>,
+  Cheesable<2 | 3 | 4>,
+  Meatable<2 | 3 | 4>,
+  Lettuceable,
+  Tomatoeable,
+  Spreadable,
+  Partial<Chilliable>,
+  Partial<GrilledOnionable>,
+  Partial<Onionable>,
+  Partial<WholeGrilledOnionable>,
+  Partial<RawOnionable>,
+  Partial<RawChoppedOnionable>,
+  Partial<ChoppedOnionable> {}
